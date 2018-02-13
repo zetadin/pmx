140e5befbd6f2411f78c967b6b18e5a16e63f7e4
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2018-02-13 13:16:51 +0100

    small update

--- setup.py
+++ setup.py
@@ -1,32 +1,11 @@
-# pmx  Copyright Notice
-# ============================
-#
-# The pmx source code is copyrighted, but you can freely use and
-# copy it as long as you don't change or remove any of the copyright
-# notices.
-#
-# ----------------------------------------------------------------------
-# pmx is Copyright (C) 2006-2013 by Daniel Seeliger
-#
-#                        All Rights Reserved
-#
-# Permission to use, copy, modify, distribute, and distribute modified
-# versions of this software and its documentation for any purpose and
-# without fee is hereby granted, provided that the above copyright
-# notice appear in all copies and that both the copyright notice and
-# this permission notice appear in supporting documentation, and that
-# the name of Daniel Seeliger not be used in advertising or publicity
-# pertaining to distribution of the software without specific, written
-# prior permission.
-#
-# DANIEL SEELIGER DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
-# SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
-# FITNESS.  IN NO EVENT SHALL DANIEL SEELIGER BE LIABLE FOR ANY
-# SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
-# RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
-# CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
-# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-# ----------------------------------------------------------------------
+"""pmx: a toolkit for free-energy calculation setup/analysis and
+biomolecular structure handling.
+
+For installation type the command::
+  python setup.py install
+or
+  pip install .
+"""
 
 from setuptools import setup, Extension
 
@@ -51,7 +30,8 @@ xdrio = Extension('pmx/_xdrio',
 
 setup(name='pmx',
       version='1.1.0dev',
-      description='Toolkit for free-energy calculation setup/analysis and biomolecular structure handling',
+      description='Toolkit for free-energy calculation setup/analysis '
+                  'and biomolecular structure handling',
       long_description=readme(),
       classifiers=[
         'Development Status :: 3 - Alpha',
