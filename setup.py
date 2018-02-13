5d6505d40ce95db276287ae2ce13aa69d8a14cf7
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2018-02-13 12:03:23 +0100

    begin package restructure and update of setup script
    data files placed within the pmx folder. this makes sure the 'data' folder is installed as package data so to make things easier for distribution

--- setup.py
+++ setup.py
@@ -28,62 +28,46 @@
 # CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 # ----------------------------------------------------------------------
 
-import os,sys
-from distutils.core import setup, Extension
-from distutils.command.install_data import install_data
+from setuptools import setup, Extension
 
-class install_data_files(install_data):
 
-    def run(self):
-        install_cmd = self.get_finalized_command('install')
-        self.install_dir = getattr(install_cmd, 'install_lib')
-        return install_data.run(self)
-
+def readme():
+    with open('README.md') as f:
+        return f.read()
 
 pmx = Extension('pmx/_pmx',
-                   libraries = ['m'],
-                   include_dirs = ['src/pmx'],
-                   sources = ['src/pmx/Geometry.c','src/pmx/wrap_Geometry.c',
-                              'src/pmx/init.c', 'src/pmx/Energy.c']
-                   
-                 )
+                libraries=['m'],
+                include_dirs=['src/pmx'],
+                sources=['src/pmx/Geometry.c', 'src/pmx/wrap_Geometry.c',
+                         'src/pmx/init.c', 'src/pmx/Energy.c']
+                )
 
 xdrio = Extension('pmx/_xdrio',
-                   libraries = ['m'],
-                   include_dirs = ['src/xdr'],
-                   sources = ['src/xdr/xdrfile.c','src/xdr/xdrfile_trr.c',
-                              'src/xdr/xdrfile_xtc.c']
-                   
-                 )
+                  libraries=['m'],
+                  include_dirs=['src/xdr'],
+                  sources=['src/xdr/xdrfile.c', 'src/xdr/xdrfile_trr.c',
+                           'src/xdr/xdrfile_xtc.c']
+                  )
 
-## cpp_test = Extension('pmx/_cpp_test',
-##                    libraries = ['m'],
-##                    include_dirs = ['src'],
-##                    sources = ['src/cpp_test.cpp']
-                   
-##                  )
-
-
-
-setup (name = 'pmx',
-       version = '1.1.0dev',
-       description = 'Python Toolbox structure file editing and writing simulation setup/analysis tools',
-       author = 'Daniel Seeliger',
-       author_email = 'seeliger.biosoft@gmail.de',
-       url = 'https://github.com/dseeliger/pmx/',
-       long_description = '''Tools to play with structure files, topologies, index files, xtc files, etc....''',
-       packages = ['pmx'],
-       data_files = [('pmx/data',['data/bbdep.pkl']),
-                     ('pmx/data',['data/bp.pkl']),
-                     #('pmx/data',['data/fragments.pkl']),
-                     #('pmx/data',['data/ffamber99sb.rtp']),
-                     #('pmx/data',['data/ffamber99sbbon.itp']),
-                     #('pmx/data',['data/ffamber99sbnb.itp']),
-                     ('pmx/data',['data/blosum62_new.mat'])
-                     ],
-       ext_modules = [pmx,xdrio],
-       cmdclass = {'install_data': install_data_files
-                   },
-
-       )
-
+setup(name='pmx',
+      version='1.1.0dev',
+      description='Toolkit for free-energy calculation setup/analysis and biomolecular structure handling',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+      ],
+      url='https://github.com/dseeliger/pmx/',
+      author='Daniel Seeliger',
+      author_email='seeliger.biosoft@gmail.de',
+      license='GPL 3',
+      packages=['pmx'],
+      package_dir={'pmx': 'pmx'},
+      include_package_data=True,
+      package_data={'pmx': ['data/*', 'data/*/*', 'data/*/*/*']},
+      zip_safe=False,
+      ext_modules=[pmx, xdrio]
+      )
