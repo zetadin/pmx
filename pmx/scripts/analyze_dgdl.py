cba049dd3e2bb4baefb576aba75578265d6a33f3
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2018-02-14 16:12:43 +0100

    use warnings

--- pmx/scripts/analyze_dgdl.py
+++ pmx/scripts/analyze_dgdl.py
@@ -379,13 +379,13 @@ def main(args):
             filesBA = [filesBA[i] for i in args.index if i < len(filesBA)]
             # ...but warn if this happens
             if any(i > (len(filesAB) - 1) for i in args.index):
-                print('\nWARNING: index out of range for some of your chosen '
-                      '\nindices for the forward work values. This means you are'
-                      '\ntrying to select input files that are not present.')
+                warnings.warn('\nWARNING: index out of range for some of your chosen '
+                              '\nindices for the forward work values. This means you are'
+                              '\ntrying to select input files that are not present.')
             if any(i > (len(filesBA) - 1) for i in args.index):
-                print('\nWARNING: index out of range for some of your chosen'
-                      '\nindices for the reverse work values. This means you are'
-                      '\ntrying to select input files that are not present.')
+                warnings.warn('\nWARNING: index out of range for some of your chosen'
+                              '\nindices for the reverse work values. This means you are'
+                              '\ntrying to select input files that are not present.')
 
         # when skipping start count from end: in this way the last frame is
         # always included, and what can change is the first one
