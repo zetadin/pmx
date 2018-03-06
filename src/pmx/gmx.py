ddee6450712700f303a25aee391d5643c4f2574c
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2018-03-06 11:37:29 +0100

    create gmx module

---
+++ src/pmx/gmx.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""This module contains wrapper functions for some of the most used Gromacs
+tools.
+"""
+
+
+def pdb2gmx():
+    pass
+
+
+def solvate():
+    pass
+
+
+def genion():
+    pass
+
+
+def grompp():
+    pass
+
+
+def trjconv():
+    pass
