4ea4f64b76e2bcc45d5d257877b28db6936b526b
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2018-03-16 10:45:29 +0100

    initialise abfe setup script

---
+++ src/pmx/scripts/setup_abfe.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+Script to setup absolute binding free energy calculations for ligands binding to proteins.
+"""
+
+# merge gro
+
+# merge top
+
+# figure out restraints (save top entry)
+
+# gmx setup: editconf, solvate, genion
+
+# add restraints to top
+
+# setup standard folder structure, write mdp files, write example bash on how to run calcs
