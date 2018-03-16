e244a7317cc5be32207d82bbbd6cc3842cc842d4
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2018-03-16 10:45:42 +0100

    add simple gmx solvate wrapper

--- src/pmx/gmx.py
+++ src/pmx/gmx.py
@@ -18,40 +18,64 @@ def get_gmx():
         raise EnvironmentError('gmx executable not found')
 
 
-def pdb2gmx(f='input.pdb', o='pdb2gmx.gro', p='topol.top', ff='amber99sb-mut',
+def pdb2gmx(f, o='pdb2gmx.gro', p='topol.top', ff='amber99sb-star-ildn-mut',
             water='tip3p', other_flags=''):
-    """Simple pdb2gmx wrapper.
+    """Simple ``gmx pdb2gmx`` wrapper.
 
     Parameters
     ----------
     f : str
-        input structure
-    o : str
-        output structure file
-    p : str
-        name of topology file
-    ff : str
-        forcefield
-    water : str
-        water model
-    other_flags : str
-        additional flags as a string. These will be given to the
-        ``gmx pdb2gmx`` command
+        input structure file
+    o : str, optional
+        output structure file. Default is "pdb2gmx.gro".
+    p : str, optional
+        name of topology file. Default is "topol.top".
+    ff : str, optional
+        forcefield. Default is "amber99sb-star-ildn-mut".
+    water : str, optional
+        water model. Default is "tip3p".
+    other_flags : str, optional
+        additional flags to pass as you would type them in the shell
 
     Returns
     -------
     None
 
     """
-    gmx = get_gmx()
 
+    gmx = get_gmx()
     call('{gmx} pdb2gmx -f {f} -o {o} -p {p} -ff {ff} -water {water} '
          '{other_flags}'.format(gmx=gmx, f=f, o=o, p=p, ff=ff, water=water, other_flags=other_flags),
          shell=True)
 
 
-def solvate():
-    pass
+def solvate(cp, cs='spc216.gro', p='topol.top', o='solvate.gro',
+            other_flags=''):
+    """Simple ``gmx solvate`` wrapper.
+
+    Parameters
+    ----------
+    cp : str
+        input structure file
+    cs : str, optional
+        structure file of the solvent. Default is "spc216.gro".
+    p : str, optional
+        name of topology file. Default is "topol.top"
+    o : str, optional
+        name of output structure file. Default is "solvate.gro"
+    other_flags : str, optional
+        additional flags to pass as you would type them in the shell
+
+    Returns
+    -------
+    None
+
+    """
+
+    gmx = get_gmx()
+    call('{gmx} solvate -cp {cp} -cs {cs} -p {p} -o {o} '
+         '{other_flags}'.format(gmx=gmx, cp=cp, cs=cs, p=p, o=o, other_flags=other_flags),
+         shell=True)
 
 
 def genion():
