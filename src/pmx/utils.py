82a5e9177f8c1cb4c6c7179ef7b506d51029c7d8
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2018-03-16 10:15:03 +0100

    add which func

--- src/pmx/utils.py
+++ src/pmx/utils.py
@@ -426,6 +426,36 @@ def initialise_logger(logname, logfile, level='INFO'):
     return logger
 
 
+def which(program):
+    """Equivalent of UNIX which command. Returns the path of the executable
+    if it is found, otherwise returns None.
+
+    Parameters
+    ----------
+    program : str
+        name of executable you are looking for
+
+    Returns
+    -------
+    path : str
+        string of path if found, otherwise None.
+
+    """
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
 # ========================
 # Custom Exception Classes
 # ========================
