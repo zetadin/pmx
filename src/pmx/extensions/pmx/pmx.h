5ba5e89a831cf9b9915857bcb4c6a72e006f3b18
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2018-07-04 13:47:09 +0200

    fix PyBytes vs PyUnicode bug
    this caused some strings to be written as b'' in py3

--- src/pmx/extensions/pmx/pmx.h
+++ src/pmx/extensions/pmx/pmx.h
@@ -51,7 +51,7 @@ typedef int             bool;
  #if PY_MAJOR_VERSION >= 3
     #define PyInt_FromLong PyLong_FromLong
     #define PyInt_AsLong PyLong_AsLong
-    #define PyString_FromString PyBytes_FromString
+    #define PyString_FromString PyUnicode_FromString
     #define PyString_AsString PyBytes_AsString
  #endif
 
